aed702552275b228289bf3b04d02e68b053b7e5a
최승탁 <tmdxkr5@hanmail.net>
2024-05-16 10:09:45 +0900

    [db] : update

--- DB/finalPJT.sql
+++ DB/finalPJT.sql
@@ -28,9 +28,9 @@ CREATE TABLE IF NOT EXISTS `finalMountainPJT`.`User` (
   `reg_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `point` INT NULL DEFAULT 0,
   PRIMARY KEY (`serial`),
-  UNIQUE INDEX `id_UNIQUE` (`serial` ASC) ,
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) ,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) )
+  UNIQUE INDEX `id_UNIQUE` (`serial` ASC) VISIBLE,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
@@ -50,9 +50,9 @@ CREATE TABLE IF NOT EXISTS `finalMountainPJT`.`Mountain` (
   `state` VARCHAR(45) NOT NULL,
   `town` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`serial`),
-  UNIQUE INDEX `id_UNIQUE` (`serial` ASC) ,
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC) ,
-  UNIQUE INDEX `course_UNIQUE` (`course` ASC) )
+  UNIQUE INDEX `id_UNIQUE` (`serial` ASC) VISIBLE,
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE,
+  UNIQUE INDEX `course_UNIQUE` (`course` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
@@ -60,12 +60,12 @@ ENGINE = InnoDB;
 -- Table `finalMountainPJT`.`ChatManager`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `finalMountainPJT`.`ChatManager` (
-  `serial` INT NOT NULL  AUTO_INCREMENT,
+  `serial` INT NOT NULL AUTO_INCREMENT,
   `mountain_serial` INT NOT NULL,
   `date` TIMESTAMP NOT NULL,
   PRIMARY KEY (`serial`),
-  UNIQUE INDEX `serial_UNIQUE` (`serial` ASC) ,
-  INDEX `fk_mountain_serial_idx` (`mountain_serial` ASC) ,
+  UNIQUE INDEX `serial_UNIQUE` (`serial` ASC) VISIBLE,
+  INDEX `fk_mountain_serial_idx` (`mountain_serial` ASC) VISIBLE,
   CONSTRAINT `fk_mountain_serial`
     FOREIGN KEY (`mountain_serial`)
     REFERENCES `finalMountainPJT`.`Mountain` (`serial`)
@@ -78,15 +78,15 @@ ENGINE = InnoDB;
 -- Table `finalMountainPJT`.`Chat`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `finalMountainPJT`.`Chat` (
-  `serial` INT NOT NULL  AUTO_INCREMENT,
+  `serial` INT NOT NULL AUTO_INCREMENT,
   `chatmanager_serial` INT NULL,
   `user_serial` INT NULL,
   `content` TEXT NULL,
   `create_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`serial`),
-  UNIQUE INDEX `serial_UNIQUE` (`serial` ASC) ,
-  INDEX `fk_manager_serial_idx` (`chatmanager_serial` ASC) ,
-  INDEX `fk_user_serial_idx` (`user_serial` ASC) ,
+  UNIQUE INDEX `serial_UNIQUE` (`serial` ASC) VISIBLE,
+  INDEX `fk_manager_serial_idx` (`chatmanager_serial` ASC) VISIBLE,
+  INDEX `fk_user_serial_idx` (`user_serial` ASC) VISIBLE,
   CONSTRAINT `fk_manager_serial`
     FOREIGN KEY (`chatmanager_serial`)
     REFERENCES `finalMountainPJT`.`ChatManager` (`serial`)
@@ -101,10 +101,10 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `finalMountainPJT`.`Review`
+-- Table `finalMountainPJT`.`Comment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `finalMountainPJT`.`Review` (
-  `serial` INT NOT NULL  AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `finalMountainPJT`.`Comment` (
+  `serial` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
   `mountain_serial` INT NOT NULL,
   `user_serial` INT NOT NULL,
@@ -112,12 +112,12 @@ CREATE TABLE IF NOT EXISTS `finalMountainPJT`.`Review` (
   `reg_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `update_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `calorie` INT NOT NULL DEFAULT 0,
-  `time` INT NOT NULL DEFAULT 0,
+  `turnaround` INT NOT NULL DEFAULT 0,
   `view_count` INT NOT NULL DEFAULT 0,
   PRIMARY KEY (`serial`),
-  UNIQUE INDEX `serial_UNIQUE` (`serial` ASC) ,
-  INDEX `fk_user_serial_idx` (`user_serial` ASC) ,
-  INDEX `fk_mountain_serial_idx` (`mountain_serial` ASC) ,
+  UNIQUE INDEX `serial_UNIQUE` (`serial` ASC) VISIBLE,
+  INDEX `fk_user_serial_idx` (`user_serial` ASC) VISIBLE,
+  INDEX `fk_mountain_serial_idx` (`mountain_serial` ASC) VISIBLE,
   CONSTRAINT `fk_user_serial`
     FOREIGN KEY (`user_serial`)
     REFERENCES `finalMountainPJT`.`User` (`serial`)
@@ -132,29 +132,29 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `finalMountainPJT`.`ReviewFile`
+-- Table `finalMountainPJT`.`CommentFile`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `finalMountainPJT`.`ReviewFile` (
-  `serial` INT NOT NULL  AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `finalMountainPJT`.`CommentFile` (
+  `serial` INT NOT NULL AUTO_INCREMENT,
   `user_serial` INT NOT NULL,
-  `review_serial` INT NOT NULL,
+  `comment_serial` INT NOT NULL,
   `file_name` VARCHAR(500) NOT NULL,
   `file_size` INT NOT NULL,
   `file_content_type` VARCHAR(500) NOT NULL,
   `file_url` VARCHAR(500) NOT NULL,
   `reg_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`serial`),
-  UNIQUE INDEX `serial_UNIQUE` (`serial` ASC) ,
-  INDEX `fk_user_serial_idx` (`user_serial` ASC) ,
-  INDEX `fk_review_serial_idx` (`review_serial` ASC) ,
+  UNIQUE INDEX `serial_UNIQUE` (`serial` ASC) VISIBLE,
+  INDEX `fk_user_serial_idx` (`user_serial` ASC) VISIBLE,
+  INDEX `fk_review_serial_idx` (`comment_serial` ASC) VISIBLE,
   CONSTRAINT `fk_user_serial`
     FOREIGN KEY (`user_serial`)
     REFERENCES `finalMountainPJT`.`User` (`serial`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_review_serial`
-    FOREIGN KEY (`review_serial`)
-    REFERENCES `finalMountainPJT`.`Review` (`serial`)
+    FOREIGN KEY (`comment_serial`)
+    REFERENCES `finalMountainPJT`.`Comment` (`serial`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -164,13 +164,13 @@ ENGINE = InnoDB;
 -- Table `finalMountainPJT`.`Follow`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `finalMountainPJT`.`Follow` (
-  `serial` INT NOT NULL  AUTO_INCREMENT,
+  `serial` INT NOT NULL AUTO_INCREMENT,
   `from_serial` INT NOT NULL,
   `to_serial` INT NOT NULL,
   PRIMARY KEY (`serial`),
-  UNIQUE INDEX `serial_UNIQUE` (`serial` ASC) ,
-  INDEX `fk_from_serial_idx` (`from_serial` ASC) ,
-  INDEX `fk_to_serial_idx` (`to_serial` ASC) ,
+  UNIQUE INDEX `serial_UNIQUE` (`serial` ASC) VISIBLE,
+  INDEX `fk_from_serial_idx` (`from_serial` ASC) INVISIBLE,
+  INDEX `fk_to_serial_idx` (`to_serial` ASC) INVISIBLE,
   CONSTRAINT `fk_from_serial`
     FOREIGN KEY (`from_serial`)
     REFERENCES `finalMountainPJT`.`User` (`serial`)
@@ -179,6 +179,32 @@ CREATE TABLE IF NOT EXISTS `finalMountainPJT`.`Follow` (
   CONSTRAINT `fk_to_serial`
     FOREIGN KEY (`to_serial`)
     REFERENCES `finalMountainPJT`.`User` (`serial`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `finalMountainPJT`.`Reply`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `finalMountainPJT`.`Reply` (
+  `serial` INT NOT NULL AUTO_INCREMENT,
+  `user_serial` INT NOT NULL,
+  `comment_serial` INT NOT NULL,
+  `content` TEXT NOT NULL,
+  `reg_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`serial`),
+  INDEX `fk_user_serial_idx` (`user_serial` ASC) VISIBLE,
+  INDEX `fk_comment_serial_idx` (`comment_serial` ASC) VISIBLE,
+  CONSTRAINT `fk_user_serial`
+    FOREIGN KEY (`user_serial`)
+    REFERENCES `finalMountainPJT`.`User` (`serial`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comment_serial`
+    FOREIGN KEY (`comment_serial`)
+    REFERENCES `finalMountainPJT`.`Comment` (`serial`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
