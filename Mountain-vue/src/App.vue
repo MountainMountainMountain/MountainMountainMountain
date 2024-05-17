848473e16f427f96dfb6449d2c259e56f6784bb7
최승탁 <tmdxkr5@hanmail.net>
2024-05-17 16:16:28 +0900

    update

---
+++ Mountain-vue/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import NavBar from "./components/layout/NavBar.vue";
+import Footer from "./components/layout/Footer.vue";
+
+
+
+</script>
+
+<template>
+  <div>
+    <nav-bar />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<style>
+
+</style>
