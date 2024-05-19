8e0502134b590841be761b19ca4dd9384d545cd6
lsy <aprils401y@gmail.com>
2024-05-19 21:45:38 +0900

    .

--- Mountain-vue/src/router/index.js
+++ Mountain-vue/src/router/index.js
@@ -3,23 +3,26 @@ import MountainView from "@/views/MountainView.vue";
 import CommentView from "@/views/CommentView.vue";
 import HomeView from "@/views/HomeView.vue";
 import MypageView from "@/views/MypageView.vue";
+import AccountView from "@/views/AccountView.vue";
+import Login from "@/components/Account/Login.vue";
+import SignUp from "@/components/Account/SignUp.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
       // children:[
       //   {
-      //     path: 
+      //     path:
       //   },
       // ]
     },
     {
-      path: '/mountain',
-      name: 'mountain',
+      path: "/mountain",
+      name: "mountain",
       component: MountainView,
       // children: [
       //   {
@@ -40,10 +43,10 @@ const router = createRouter({
       // ],
     },
     {
-      path: '/mypage',
-      name: 'MyPage',
+      path: "/mypage",
+      name: "MyPage",
       component: MypageView,
-      redirect: '/myPage/A/MyInfo',
+      redirect: "/myPage/A/MyInfo",
       // children: [
       //   {
       //     path: '',
@@ -60,81 +63,90 @@ const router = createRouter({
       //     name: 'myComplete',
       //     component: MyComplete,
       //   },
-        // { 이거는 모달이나 그 자체에서 바꿀까?
-        //   path: 'modifyInfo',
-        //   name: 'modifyInfo',
-        //   component: ModifyInfo,
-        // },
+      // { 이거는 모달이나 그 자체에서 바꿀까?
+      //   path: 'modifyInfo',
+      //   name: 'modifyInfo',
+      //   component: ModifyInfo,
+      // },
       // ],
     },
-  //   {
-  //     path: '/comment',
-  //     name: 'Comment',
-  //     component: CommentView,
-  //     children: [
-  //       {
-  //         path: 'create',
-  //         name: 'CommentCreate',
-  //         component: CommentCreate,
-  //       },
-  //       {
-  //         path: 'detail',
-  //         name: "CommentDetail",
-  //         component: CommentDetail,
-  //       },
-  //       {
-  //         path: 'list',
-  //         name: 'CommentList',
-  //         component: CommentDetail,
-  //       },
-  //       {
-  //         path: 'modify',
-  //         name: 'CommentModify',
-  //         component: CommentModify,
-  //       },
-        
-  //     ],
-  //   },
-  //   {
-  //     path: '/chat',
-  //     name: 'Chat',
-  //     component: ChatView,
-  //     children: [
-  //       {
-  //         path: 'detailChat',
-  //         name: 'DetailChat',
-  //         component: DetailChat,
-  //       },
-  //       {
-  //         path: 'joinChat',
-  //         name: 'JoinChat',
-  //         component: JoinChat,
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     path: '/account',
-  //     name: 'account',
-  //     component: AccountView,
-  //     redirect: '/account/login',
-  //     children: [
-  //       {
-  //         path: 'login',
-  //         name: 'Login',
-  //         component: Login,
-  //       },
-  //       {
-  //         path: 'signUp',
-  //         name: 'SignUp',
-  //         component: SignUp,
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     path: '/',
-  //   },
+    //   {
+    //     path: '/comment',
+    //     name: 'Comment',
+    //     component: CommentView,
+    //     children: [
+    //       {
+    //         path: 'create',
+    //         name: 'CommentCreate',
+    //         component: CommentCreate,
+    //       },
+    //       {
+    //         path: 'detail',
+    //         name: "CommentDetail",
+    //         component: CommentDetail,
+    //       },
+    //       {
+    //         path: 'list',
+    //         name: 'CommentList',
+    //         component: CommentDetail,
+    //       },
+    //       {
+    //         path: 'modify',
+    //         name: 'CommentModify',
+    //         component: CommentModify,
+    //       },
+
+    //     ],
+    //   },
+    //   {
+    //     path: '/chat',
+    //     name: 'Chat',
+    //     component: ChatView,
+    //     children: [
+    //       {
+    //         path: 'detailChat',
+    //         name: 'DetailChat',
+    //         component: DetailChat,
+    //       },
+    //       {
+    //         path: 'joinChat',
+    //         name: 'JoinChat',
+    //         component: JoinChat,
+    //       },
+    //     ],
+    //   },
+    // {
+    //   path: '/account',
+    //   name: 'account',
+    //   component: AccountView,
+    //   // redirect: '/account/login',
+    //   children: [
+    //     {
+    //       path: 'login',
+    //       name: 'Login',
+    //       component: Login,
+    //     },
+    //     {
+    //       path: 'signUp',
+    //       name: 'SignUp',
+    //       component: SignUp,
+    //     },
+    //   ],
+    // },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp
+    },
+      // {
+      //   path: '/',
+      // },
   ],
-}
-);
+});
 
 export default router;
