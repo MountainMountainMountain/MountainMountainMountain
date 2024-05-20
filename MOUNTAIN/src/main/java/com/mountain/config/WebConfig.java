8868c0b18a4525e1e6d8372e24cc6c8b3928ebe4
lsy <aprils401y@gmail.com>
2024-05-20 09:29:19 +0900

    1

--- MOUNTAIN/src/main/java/com/mountain/config/WebConfig.java
+++ MOUNTAIN/src/main/java/com/mountain/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.addMapping("/**").allowedOrigins("*").allowedMethods("POST", "GET", "PUT", "DELETE");
 	}
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 //	// 인터셉터 등 처리 가능
 //	@Autowired
 //	private JwtInterceptor jwtInterceptor;
@@ -34,7 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
 //	public void addInterceptors(InterceptorRegistry registry) {
 //		registry.addInterceptor(jwtInterceptor).addPathPatterns("/**").excludePathPatterns("/api-chat/**",
 //				"/api-comment/**", "/api-mountain/**", "/api-user/**", "/swagger-ui/**", "/v3/api-docs/**");
-=======
+//=======
 	// 인터셉터 등 처리 가능
 	@Autowired
 	private JwtInterceptor jwtInterceptor;
@@ -44,7 +44,7 @@ public class WebConfig implements WebMvcConfigurer {
 //		registry.addInterceptor(jwtInterceptor).addPathPatterns("/**").excludePathPatterns("/api-chat/**",
 //				"/api-comment/**", "/api-mountain/**", "/api-user/**", "/api-reply/**", "/swagger-ui/**",
 //				"/v3/api-docs/**");
->>>>>>> 45c2971ddfb3043a86233690123dd64062c40d96
+//>>>>>>> 45c2971ddfb3043a86233690123dd64062c40d96
 //	}
 
 }
