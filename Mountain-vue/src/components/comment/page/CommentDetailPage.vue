f4680dad7f02388434b327d2c9573a17e66b928d
최승탁 <tmdxkr5@hanmail.net>
2024-05-20 17:49:21 +0900

    update 240520 17:49

--- Mountain-vue/src/components/comment/page/CommentDetailPage.vue
+++ Mountain-vue/src/components/comment/page/CommentDetailPage.vue
@@ -1,12 +1,32 @@
 <template>
     <div>
         <h1>commentdetail</h1>
-        <RouterLink :to="{ name: 'CommentModifyPage', params: { commentSerial: '1' } }">modifypage</RouterLink>
+        <RouterLink :to="{ name: 'CommentModifyPage', params: { commentSerial: '`${route.params.commentSerial}`' } }">
+            modifypage</RouterLink>
+
+        <ul>
+            <span> 제목: {{ commentStore.Comment.title }}</span>
+            <span> , 작성자: {{ commentStore.Comment.name }}</span>
+            <span> , 조회 수: {{ commentStore.Comment.view_count }} </span>
+            <span> , 내용: {{ commentStore.Comment.content }} </span>
+            <!-- <RouterLink :to="{ name: 'MountainDetailPage', params: { mountainSerial: '1' } }">1</RouterLink> -->
+        </ul>
 
     </div>
 </template>
 
 <script setup>
+import { useRoute, useRouter } from "vue-router";
+import { ref, onMounted } from "vue";
+import { useCommentStore } from "@/stores/commentstore";
+import axios from "axios";
+const route = useRoute();
+const router = useRouter();
+const commentStore = useCommentStore();
+
+onMounted(() => {
+    commentStore.getComment(route.params.commentSerial)
+})
 
 </script>
 
--- Mountain-vue/src/components/mypage/component/MyState.vue
+++ Mountain-vue/src/components/mypage/component/MyState.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
-        <h1>state</h1>
-    </div>
+        <div class="breadcrumb-item active">마이 페이지</div>
+        <img style="width: 50px; border-radius: 30%;" src="@/assets/images/mountain1.jpg" alt="캐릭터">
+        <!-- {{ UserStore. }} -->
+        </div>
+
+        
 </template>
 
 <script setup>
+// import { useUserStore } from '@/stores/userstore';
+// import { onMounted } from 'vue';
 
+// const UserStore = useUserStore();
+
+// onMounted(() => {
+//     UserStore.getUser();
+// })
 </script>
 
 <style scoped>
