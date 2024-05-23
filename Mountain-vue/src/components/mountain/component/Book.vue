5bcdf947c2fbdefe145f14af47d8cb1b8f046c6f
lsy <aprils401y@gmail.com>
2024-05-23 10:39:38 +0900

    0523 10:39

--- Mountain-vue/src/components/mountain/component/Book.vue
+++ Mountain-vue/src/components/mountain/component/Book.vue
@@ -1,33 +1,37 @@
 <template>
-    <div class="container">
-        <Weather />
-        <div class="wrapper">
-            <header>
-                <div class="nav">
-                    <button @click="prevMonth" class="material-icons"> <i class="bi bi-caret-left-fill"></i> </button>
-                    <p class="current-date">{{ currentDate }}</p>
-                    <button @click="nextMonth" class="material-icons"> <i class="bi bi-caret-right-fill"></i> </button>
-                </div>
-            </header>
-            <div class="calendar">
-                <ul class="weeks">
-                    <li>Sun</li>
-                    <li>Mon</li>
-                    <li>Tue</li>
-                    <li>Wed</li>
-                    <li>Thu</li>
-                    <li>Fri</li>
-                    <li>Sat</li>
-                </ul>
-                <ul class="days">
-                    <li v-for="day in days" :key="day.date" :class="{ inactive: day.inactive, active: day.isToday }" @click="selectDate(day.date)">
-                        {{ day.date }}
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
+  <div class="bookbox">
+      <div class="weather-container">
+          <Weather />
+      </div>
+      <div class="calendar-container">
+          <header>
+              <div class="nav">
+                  <button @click="prevMonth" class="material-icons"> <i class="bi bi-caret-left-fill"></i> </button>
+                  <p class="current-date">{{ currentDate }}</p>
+                  <button @click="nextMonth" class="material-icons"> <i class="bi bi-caret-right-fill"></i> </button>
+              </div>
+          </header>
+          <div class="calendar">
+              <ul class="weeks">
+                  <li>Sun</li>
+                  <li>Mon</li>
+                  <li>Tue</li>
+                  <li>Wed</li>
+                  <li>Thu</li>
+                  <li>Fri</li>
+                  <li>Sat</li>
+              </ul>
+              <ul class="days">
+                  <li v-for="day in days" :key="day.date" :class="{ inactive: day.inactive, active: day.isToday }" @click="selectDate(day.date)">
+                      {{ day.date }}
+                  </li>
+              </ul>
+          </div>
+      </div>
+  </div>
 </template>
+
+
 
 <script setup>
 import Weather from '@/components/mountain/component/Weather.vue';
@@ -126,47 +130,61 @@ const selectDate = (date) => {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'Poppins', sans-serif;
+  font-family: "Gowun Dodum", sans-serif;
+}
+.material-icons {
+  border: none;
+  background-color: white;
 }
 
 body {
   display: flex;
   align-items: center;
   justify-content: center;
-  min-height: 100vh;
-  background: #008aff;
+  height: 300vh; /* 배경 높이를 더 키움 */
+  background: rgb(246, 246, 246);
 }
 
-.container {
+.bookbox {
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  width: 100%;
+  align-items: center; /* 세로 중앙 정렬 */
+  justify-content: center; /* 가로 중앙 정렬 */
+  background: rgb(246, 246, 246);
+  height: 80vh;
 }
 
-.wrapper {
-  width: 500px;
+.weather-container {
+  width: 30%;
+  padding: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  margin-right: 10px;
+}
+
+.calendar-container {
+  width: 1100px; /* 달력 크기를 줄임 */
+  height: 500px;
   background: #fff;
   border-radius: 10px;
-  padding: 30px;
+  padding: 50px;
+  padding-left: 5%;
+  padding-right: 5%;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
-.wrapper .nav {
+.calendar-container .nav {
   display: flex;
   align-items: center;
   justify-content: space-between;
   margin-bottom: 30px;
 }
 
-.wrapper .nav .current-date {
+.calendar-container .nav .current-date {
   font-size: 24px;
   font-weight: 600;
 }
 
-.wrapper .nav button {
+.calendar-container .nav button {
   width: 38px;
   height: 38px;
   font-size: 30px;
@@ -227,3 +245,5 @@ body {
   background: #008aff;
 }
 </style>
+
+
